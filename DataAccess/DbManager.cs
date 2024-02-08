032890f712f851b3d12b2fe8709cc279848bd438
The Real G <dcreolese@gmail.com>
2024-02-08 21:53:33 +0100

    CRUD methoden erstellt und SQL befehle angefangen

--- DataAccess/DbManager.cs
+++ DataAccess/DbManager.cs
@@ -6,9 +6,25 @@ namespace DataAccess
     public static class DbManager
     {
       
-        public static void datensatzLoeschen()
+        public static void loesceDantensatz()
         {
+            string loeschString = "DELETE FROM produkt WHERE id=";
+        }
 
+        public static void holenDatensatz()
+        {
+            string selectString = "SELECT id, Produktname, Beschreibung, Artikelnummer, Kategorie, Preis, Verfügbarkeit, Lagermenge FROM produkt";
+        }
+
+
+        public static void fuegeDatensatzHinzu()
+        {
+            string hinzufuegenString = "INSERT INTO produkt(Produktname, Beschreibung, Artikelnummer, Kategorie, Preis, Verfügbarkeit, Lagermenge) VALUES ()";
+        }
+
+        public static void updateDatenzsatz()
+        {
+            string updateString = "UPDATE produkt SET ";
         }
     }
 }
